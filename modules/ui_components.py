04ec218411138418c058a89fe34078580a5134fd
ta-bear <katakuma4625@gmail.com>
2025-04-18 21:12:06 +0900

    fix some unshowed situation

--- modules/ui_components.py
+++ modules/ui_components.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from modules.plotting_modules import *
+from tkhtmlview import HTMLLabel
+from modules.plotting_modules import (
+    plot_speed_telemetry, plot_session_info, plot_circuit_map, plot_laptime_comparison,
+    plot_position_changes, plot_points_bar, plot_track_heatmap, plot_qualy_bubble,
+    plot_tire_strategy, plot_team_pace
+)
 
 class Sidebar(tk.Frame):
     def __init__(self, parent, **kwargs):
@@ -10,7 +15,7 @@ class Sidebar(tk.Frame):
 
     def _build(self):
         ttk.Label(self, text="Year:").pack(anchor='w', pady=5)
-        self.year_cb = ttk.Combobox(self, values=[2022,2023,2024,2025], state='readonly')
+        self.year_cb = ttk.Combobox(self, values=[1950,1951,1952,1953,1954,1955,1956,1957,1958,1959,1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024,2025], state='readonly')
         self.year_cb.current(3)
         self.year_cb.pack(fill='x')
 
@@ -20,7 +25,7 @@ class Sidebar(tk.Frame):
 
         ttk.Label(self, text="Session:").pack(anchor='w', pady=5)
         self.ses_cb = ttk.Combobox(self, values=['FP1','FP2','FP3','Q','R'], state='readonly')
-        self.ses_cb.current(3)
+        self.ses_cb.current(4)
         self.ses_cb.pack(fill='x')
 
         ttk.Label(self, text="Drivers:").pack(anchor='w', pady=5)
@@ -28,7 +33,6 @@ class Sidebar(tk.Frame):
         self.drivers_lb.pack(fill='x')
 
         self.load_btn = ttk.Button(self, text="Load")
-        self.load_btn.pack(fill='x', pady=10)
         self.load_btn.pack(fill='x', pady=10)
 
         self.progress = ttk.Progressbar(self, mode='indeterminate')
@@ -57,8 +61,10 @@ class Sidebar(tk.Frame):
         self.drivers_lb.delete(0, 'end')
         for d in sorted(ses.drivers):
             self.drivers_lb.insert('end', d)
-        self.mt.show_all(ses, [self.drivers_lb.get(i) for i in self.drivers_lb.curselection()] or [ses.drivers[0]])
         self.drivers_lb.bind('<<ListboxSelect>>', lambda e: self.mt.show_all(ses, [self.drivers_lb.get(i) for i in self.drivers_lb.curselection()]))
+        # Display all tabs with default selection
+        selected = [self.drivers_lb.get(i) for i in self.drivers_lb.curselection()] or [sorted(ses.drivers)[0]]
+        self.mt.show_all(ses, selected)
 
 class MainTab(tk.Frame):
     def __init__(self, parent, **kwargs):
@@ -66,9 +72,48 @@ class MainTab(tk.Frame):
         self._build()
 
     def _build(self):
-        self.notebook = ttk.Notebook(self)
-        # 必要なタブを追加
-        for name in ['Telemetry','SessionInfo','CircuitMap','LapComp','PosChange','Points','Heatmap','QualyBubble','Strategy','TeamPace']:
-            frame = ttk.Frame(self.notebook)
-            self.notebook.add(frame, text=name)
-        self.notebook.pack(fill='both', expand=True)
+        self.nb = ttk.Notebook(self)
+        self.tabs = {}
+        tab_names = ['Telemetry','SessionInfo','CircuitMap','LapComp','PosChange','Points','Heatmap','QualyBubble','Strategy','TeamPace']
+        for name in tab_names:
+            frame = ttk.Frame(self.nb)
+            self.nb.add(frame, text=name)
+            self.tabs[name] = frame
+        self.nb.pack(fill='both', expand=True)
+
+    def clear(self, tab):
+        for widget in tab.winfo_children():
+            widget.destroy()
+
+    def embed_fig(self, fig, tab):
+        self.clear(tab)
+        canvas = FigureCanvasTkAgg(fig, master=tab)
+        canvas.draw()
+        canvas.get_tk_widget().pack(fill='both', expand=True)
+
+    def show_all(self, ses, drivers):
+        # Telemetry
+        self.embed_fig(plot_speed_telemetry(ses, drivers[0]), self.tabs['Telemetry'])
+        # Session Info
+        self.embed_fig(plot_session_info(ses), self.tabs['SessionInfo'])
+        # Circuit Map
+        self.embed_fig(plot_circuit_map(ses), self.tabs['CircuitMap'])
+        # Lap Time Comparison
+        self.embed_fig(plot_laptime_comparison(ses, drivers[:4]), self.tabs['LapComp'])
+        # Position Changes
+        self.embed_fig(plot_position_changes(ses), self.tabs['PosChange'])
+        # Points Bar
+        self.embed_fig(plot_points_bar(ses), self.tabs['Points'])
+        # Speed Heatmap
+        self.embed_fig(plot_track_heatmap(ses), self.tabs['Heatmap'])
+        # Qualy Bubble (interactive)
+        self.clear(self.tabs['QualyBubble'])
+        fig = plot_qualy_bubble(ses, drivers[:4])
+        html = fig.to_html(include_plotlyjs='cdn')
+        html_label = HTMLLabel(self.tabs['QualyBubble'], html=html)
+        html_label.pack(fill='both', expand=True)
+        # Team Pace
+        self.embed_fig(plot_team_pace(ses), self.tabs['TeamPace'])
+        self.embed_fig(plot_tire_strategy(ses), self.tabs['Strategy'])
+        # Team Pace
+        self.embed_fig(plot_team_pace(ses), self.tabs['TeamPace'])
